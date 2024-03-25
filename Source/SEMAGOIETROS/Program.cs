ea42030c0cd2ae9cfb736534209db0c531e5841c
FL <felip3.fl@gmail.com>
2024-03-25 20:34:36 -0300

    Update project

--- Source/SEMAGOIETROS/Program.cs
+++ Source/SEMAGOIETROS/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Files;
+
 Console.WriteLine("Hello, World!");
+
+Csv csv = new Csv();
+
+var result = csv.OpenCsv(@"C:\Users\felip\OneDrive\Documents\Games\My game list.csv");
