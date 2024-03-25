576335b0bff21d5d037cf14acd523f6ff7fe1d20
FL <felip3.fl@gmail.com>
2024-03-25 20:33:59 -0300

    New project Csv

---
+++ Source/Files/Csv.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Files
+{
+    public class Csv
+    {
+        public List<string[]> OpenCsv(string pathFile)
+        {
+            string[] lines = File.ReadAllLines(pathFile);
+            List<string> linesList = lines.ToList();
+
+            return linesList.Select(line => line.Split(';')).ToList();
+        }
+    }
+}
