9c965f704fc388810ac761bcec16d9b7133013e0
FL <felip3.fl@gmail.com>
2024-04-30 21:32:55 -0300

    Test read excel file in Program

--- Source/SEMAGOIETROS/Program.cs
+++ Source/SEMAGOIETROS/Program.cs
@@ -1,10 +1,18 @@
 ﻿using Files;
+using OfficeFile;
+using System.Text;
+
+
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
 
 Console.WriteLine("Game sorteado:");
 
 Csv csv = new Csv();
+ExcelFile excelFile = new ExcelFile();
 
-var result = csv.OpenCsv(@"C:\Users\felip\OneDrive\Documents\Games\My game list.csv");
+var result = csv.OpenCsv(@"C:\Users\felip\OneDrive\Documents\My games\My game list.csv");
+var result2 = excelFile.Open(@"C:\Users\felip\OneDrive\Documents\My games\My game list.xlsx");
 
 
 //print a radom number, the max number is result count
