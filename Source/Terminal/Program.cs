ad46f5ce923cdf341ab6b9488609af769f80b827
kadu <kadukaua@gmail.com>
2024-07-28 15:51:22 -0300

    005 - Melhoria do sistema de contagem

--- Source/Terminal/Program.cs
+++ Source/Terminal/Program.cs
@@ -5,19 +5,20 @@ using Terminal;
 
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
+AscNumber ascNumber = new AscNumber();
+
 string choice = "S";
 string filePath = @"Files\My game list.xlsx";
+
+
 if (File.Exists(filePath) == true) {
 
-    for (int number = 5; number >= 1; number -- )  {
+    Console.Clear();
+    Console.WriteLine("Adicione quantos jogadores você deseja: ");
+    int players = Convert.ToInt32(Console.ReadLine());
 
-        var count = new AscFont();
-        List<string> numbers = count.Numbers;
-        Console.WriteLine(numbers[number]);
-        await Task.Delay(1000);
-        Console.Clear();
+    countSystem();
 
-    }
 
     while (choice == "S" || choice == "s") {
 
@@ -30,12 +31,16 @@ if (File.Exists(filePath) == true) {
         var result = excelFile.Open(@"Files\My game list.xlsx");
 
         
-
+        // _gameList[10]
 
         //print a radom number, the max number is result count
-        var game = result[new Random().Next(0, result.Count)];
+        var numeroPosicaoSorteado = new Random().Next(0, result.Count);
+        var game = result[numeroPosicaoSorteado];
+
         Console.WriteLine("Name: " + game[0]);
+        // Console.WriteLine("Name: " + _gameList[4].Name);
         Console.WriteLine("Store: " + game[4]);
+        //Console.WriteLine("Store: " +  _gameList[4].Store);
         Console.WriteLine("Player: " + game[1]);
         Console.WriteLine("Only Local: " + game[2]);
         Console.WriteLine("Better together: " + game[3]);
@@ -49,6 +54,17 @@ if (File.Exists(filePath) == true) {
     Console.WriteLine("Erro");
 
     Console.ReadKey();
+}
+
+void countSystem() {
+
+    for (int number = 5; number >= 1; number -- )  {
+
+    Console.WriteLine(ascNumber.Numbers[number]);
+    Thread.Sleep(1000);
+    Console.Clear();
+
+    }
 }
 
 public class AscFont : AscNumber {
